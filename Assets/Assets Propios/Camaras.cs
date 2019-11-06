37a78f78054cc9191f9c7ceddd0fd8453478d060
Xabier <48485280+imZavo@users.noreply.github.com>
2019-11-06 09:57:18 +0100

    Subida Proyecto

---
+++ Assets/Assets Propios/Camaras.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camaras : MonoBehaviour
+{
+    public Camera cam1, cam2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            cam1.enabled = true;
+            cam2.enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            cam1.enabled = false;
+            cam2.enabled = true;
+        }
+
+    }
+
+
+
+}
